344521266b9d9739c49b6e54175f14fb72820d7a
dyesria <dyeskorea0131@gmail.com>
2026-09-13 23:25:07 +0900

    [Passed] 차례로 출력 (100ms, 9MB)

---
+++ trail1/차례로 출력/print-in-order.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        for(int i=5; i<=17; i++) {
+            if (i != 17) {
+                System.out.print(i + " ");
+                continue;
+            }
+
+            if (i == 17) {
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+}
