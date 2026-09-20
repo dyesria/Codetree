3d8f52fe09a6ad29914fb7a661cda6b8c7c76dca
dyesria <dyeskorea0131@gmail.com>
2026-09-20 23:39:13 +0900

    [Passed] 소수점 반올림하기 (2ms, 0MB)

---
+++ trail1/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+
+    cout.precision(1);
+
+    double a = 25.352;
+
+    cout << a;
+    
+    return 0;
+}
