5564497426805738cdd22f37192033c7530dab35
dyesria <dyeskorea0131@gmail.com>
2026-09-20 23:19:05 +0900

    [Passed] 변수 출력하기 (2ms, 0MB)

---
+++ trail1/변수 출력하기/outputing-variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    string b = "C";
+
+    cout << a << "..." << b;
+    
+    return 0;
+}
