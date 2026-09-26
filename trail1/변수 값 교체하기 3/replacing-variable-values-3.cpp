558aca7c577ee8e93d5065c7d7452fe1c9ea3f83
dyesria <dyeskorea0131@gmail.com>
2026-09-26 17:18:12 +0900

    [Passed] 변수 값 교체하기 3 (2ms, 0MB)

---
+++ trail1/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    int b = 5;
+    int tmp = a;
+
+    a = b;
+    b = tmp;
+
+    cout << a << endl << b;
+    return 0;
+}
